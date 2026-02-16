af8d2285c2a2668d79a73a52a999de4216b0fa24
Dennis Gonzalez <degonzalez376@gmail.com>
2026-02-15 20:40:22 -0500

    Primer commit actividad C#

---
+++ Prueba/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Prueba;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 25;
+        double height = 1.75;
+        bool is_student = true;
+        string name = "Ana";
+        char grade = 'A';
+
+
+       Console.WriteLine("La edad es: " + age);
+       Console.WriteLine("La altura es: " + height);
+       Console.WriteLine("Es estudiante: " + is_student);
+       Console.WriteLine("El nombre es: " + name);
+       Console.WriteLine("La calificación es: " + grade);
+    }
+}
---
+++ Prueba2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Prueba2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int width = 7;
+        int height = 5;
+        int area = width * height;
+        int perimeter =( 2 * width + 2 * height);
+        width += 3;
+        height -=2;
+        area = width * height;
+        perimeter = ( 2 * width + 2 * height);
+
+        
+
+        Console.WriteLine("El ancho final es: " + width);
+        Console.WriteLine("La altura final es: " + height);
+        Console.WriteLine("El área final es: " + area);
+        Console.WriteLine("El perímetro final es: " + perimeter);
+    }
+}
